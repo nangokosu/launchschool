86db4b841468b7d1f28b190bcf92c63b4dd72995
Khai Nguyen <khainguyen@Khais-MacBook-Pro.local>
2025-12-29 15:46:57 -0800

    refactor calculator

--- calculator.py
+++ calculator.py
@@ -1,9 +1,33 @@
+def prompt(message):
+    return f"=>{message}"
+
+def invalid_number(number_str):
+    try:
+        int(number_str)
+    except ValueError:
+        return True
+
+    return False
+
+
+
 if __name__ == "__main__":
     print("Welcome to the calculator!")
-    number1 = input("What's the first number? ")
-    number2 = input("What's the second number? ")
+    print(prompt("What's the first number? "))
+    number1 = input()
+    while invalid_number(number1):
+        print("Not valid number")
+        number1 = input()
+    print(prompt("What's the second number? "))
+    number2 = input()
+    while invalid_number(number2):
+        print("Not valid number")
+        number2 = input()
     print('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide')
     operation = input()
+    while operation not in ["1", "2", "3", "4"]:
+        print(prompt('You must choose 1, 2, 3, or 4'))
+        operation = input()
 
 
     if operation == '1':   # '1' represents addition
