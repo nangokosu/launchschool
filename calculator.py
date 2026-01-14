f99fe868e4da5e2bc0c9aed8dc63070a58b6f886
Khai Nguyen <khainguyen@Khais-MacBook-Pro.local>
2026-01-13 23:32:33 -0800

    Adding retry

--- calculator.py
+++ calculator.py
@@ -12,31 +12,42 @@ def invalid_number(number_str):
 
 
 if __name__ == "__main__":
-    print("Welcome to the calculator!")
-    print(prompt("What's the first number? "))
-    number1 = input()
-    while invalid_number(number1):
-        print("Not valid number")
+    
+    while True: 
+        print("Welcome to the calculator!")
+        
+        print(prompt("What's the first number? "))
         number1 = input()
-    print(prompt("What's the second number? "))
-    number2 = input()
-    while invalid_number(number2):
-        print("Not valid number")
+        while invalid_number(number1):
+            print("Not valid number")
+            number1 = input()
+        
+        print(prompt("What's the second number? "))
         number2 = input()
-    print('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide')
-    operation = input()
-    while operation not in ["1", "2", "3", "4"]:
-        print(prompt('You must choose 1, 2, 3, or 4'))
+        while invalid_number(number2):
+            print("Not valid number")
+            number2 = input()
+            
+        
+        
+        print('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide')
         operation = input()
+        while operation not in ["1", "2", "3", "4"]:
+            print(prompt('You must choose 1, 2, 3, or 4'))
+            operation = input()
 
 
-    if operation == '1':   # '1' represents addition
-        output = int(number1) + int(number2)
-    elif operation == '2': # '2' represents subtraction
-        output = int(number1) - int(number2)
-    elif operation == '3': # '3' represents multiplication
-        output = int(number1) * int(number2)
-    elif operation == '4': # '4' represents division
-        output = int(number1) / int(number2)
+        if operation == '1':   # '1' represents addition
+            output = int(number1) + int(number2)
+        elif operation == '2': # '2' represents subtraction
+            output = int(number1) - int(number2)
+        elif operation == '3': # '3' represents multiplication
+            output = int(number1) * int(number2)
+        elif operation == '4': # '4' represents division
+            output = int(number1) / int(number2)
 
-    print(f"The result is: {output}")
+        print(f"The result is: {output}")
+        print(prompt("Do you want another session? Y/N"))
+        continue_choice = input()
+        if continue_choice.lower() == 'n':
+            break
